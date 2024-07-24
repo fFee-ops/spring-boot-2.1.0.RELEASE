3caa823b0642e0d89dc3efe84a4f0d215a1ce211
shanglei <shanglei03@corp.netease.com>
2024-07-24 16:29:17 +0800

    feat(log): add log

--- spring-boot-tests/spring-boot-ymbj-tests/spring-boot-generaldemo-tests/src/main/java/com/ymbj/ordertest/condition/ConditionalOnLinux.java
+++ spring-boot-tests/spring-boot-ymbj-tests/spring-boot-generaldemo-tests/src/main/java/com/ymbj/ordertest/condition/ConditionalOnLinux.java
@@ -3,9 +3,11 @@ package com.ymbj.ordertest.condition;
 import org.springframework.context.annotation.Conditional;
 
 import java.lang.annotation.*;
-@Target({ ElementType.TYPE, ElementType.METHOD })
+
+@Target({ElementType.TYPE, ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
+//ConditionalOnLinux注解上用了@Conditional，而LinuxCondition implements Condition，所以被@ConditionalOnLinux注解的类会被LinuxCondition的matches()方法判断
 @Conditional(LinuxCondition.class)
 public @interface ConditionalOnLinux {
 	// 标注是哪个环境
